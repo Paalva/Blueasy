b11d3becba02f78f625d455f65f678bb5f9739a6
Paalva <pablo19512@gmail.com>
2021-05-31 13:22:08 -0600

    Blueasy

---
+++ index.php
@@ -0,0 +1,274 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>BlueEasy</title>
+</head>
+
+<body>
+   
+    <div class="container-fluid">
+        <header>
+            <nav class="top-bar">
+
+                <li class="top-nav-title"><a class="top-nav-title" href="#">BLU<span class="E">E</span>ASY </a></li>
+
+
+                <ul class="top-nav">
+                    <li class="top-nav-item">HOME</li>
+                    <li class="top-nav-item">SERVICES</li>
+                    <li class="top-nav-item">FEATURES</li>
+                    <li class="top-nav-item">PORTFOLIO</li>
+                    <li class="top-nav-item">CONTACT</li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+
+
+    <!-- TEXTO CENTRAL 1-->
+    <div class="h1-fondo">
+        <h1>
+            "I’M LOOKING FOR THE UNEXPECTED.
+        </h1>
+
+        <h2>
+            I’M LOOKING FOR THE THINGS I’VE NEVER SEEN BEFORE."
+        </h2>
+    </div>
+    
+
+    <!-- TEXTO SECCIONES-->
+<section>
+    <div class="row services-part">
+        <p class="title-service">Services</p>
+        <div class="row logos">
+
+            <div class="col-sm-3">
+                <img class="logos" src="imgs/basket.png">
+                <div class="titles">Coffee</div>
+                <p class="description">
+                    Apple pie icing sweet. Brownie jelly-o applicake applicake 
+                    sweet roll liquorice bear claw. Jujubes carrot cake cotton 
+                    candy sweet tart brownie. Tiramisu applicake jujubes.
+                </p>
+            </div>
+
+            <div class="col-sm-3">
+                <img class="logos" src="imgs/polaroid.png"> 
+                <div class="titles">Instant</div>
+                <p class="description">
+                    Apple pie icing sweet. Brownie jelly-o applicake applicake 
+                    sweet roll liquorice bear claw. Jujubes carrot cake cotton 
+                    candy sweet tart brownie. Tiramisu applicake jujubes.
+                </p>
+            </div>
+
+
+            <div class="col-sm-3">
+                <img class="logos" src="imgs/camera.png">
+                <div class="titles">Serious</div>
+                <p class="description">
+                    Apple pie icing sweet. Brownie jelly-o applicake applicake 
+                    sweet roll liquorice bear claw. Jujubes carrot cake cotton 
+                    candy sweet tart brownie. Tiramisu applicake jujubes..
+                </p>
+            </div>
+            <div class="col-sm-3">
+                <img class="logos" src="imgs/picture.png">
+                <div class="titles">Frame</div>
+                <p class="description">
+                    Apple pie icing sweet. Brownie jelly-o applicake applicake 
+                    sweet roll liquorice bear claw. Jujubes carrot cake cotton 
+                    candy sweet tart brownie. Tiramisu applicake jujubes..
+                </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+<!-- TEXTO  TITULOS DE FOTOS-->
+
+    <div class="portafolio-div">
+            <nav class="phothos-bar">
+
+                <h3 class="portafolio">Portfolio</h3>
+
+                <ul class="portafolio-section">
+                    <li class="portafolio-section-item">All</li>
+                    <li class="portafolio-section-item">Panoramas</li>
+                    <li class="portafolio-section-item">Portraits</li>
+                    <li class="portafolio-section-item">Macro</li>
+                    <li class="portafolio-section-item">Journal</li>
+                </ul>
+            </nav>
+    
+
+        <!-- FOTOS -->
+        <div class="row fotos-juntas">
+            <div class="col-sm-3 imagenes">  <div class="hover align-items-center"><p class="mas">+</p></div>  <img class="imgs" src="imgs/thumbs/01.jpg"> </div>
+            <div class="col-sm-3 imagenes">  <div class="hover align-items-center"><p class="mas">+</p></div>  <img class="imgs" src="imgs/thumbs/02.jpg"> </div>
+            <div class="col-sm-3 imagenes">  <div class="hover align-items-center"><p class="mas">+</p></div>  <img class="imgs" src="imgs/thumbs/03.jpg"> </div>
+            <div class="col-sm-3 imagenes">  <div class="hover align-items-center"><p class="mas">+</p></div>  <img class="imgs" src="imgs/thumbs/04.jpg"> </div>
+            <div class="col-sm-3 imagenes">  <div class="hover align-items-center"><p class="mas">+</p></div>  <img class="imgs" src="imgs/thumbs/05.jpg"> </div>
+            <div class="col-sm-3 imagenes">  <div class="hover align-items-center"><p class="mas">+</p></div>  <img class="imgs" src="imgs/thumbs/06.jpg"> </div>
+            <div class="col-sm-3 imagenes">  <div class="hover align-items-center"><p class="mas">+</p></div>  <img class="imgs" src="imgs/thumbs/07.jpg"> </div>
+            <div class="col-sm-3 imagenes">  <div class="hover align-items-center"><p class="mas">+</p></div>  <img class="imgs" src="imgs/thumbs/01.jpg"> </div>
+        </div>
+
+    </div>
+
+
+    <!-- VIDEO Y TEXTO -->
+    <div class="row defaul-back">
+        <div class="col">
+
+        <div class="title-defoult-section">
+            Just default section
+        </div>
+
+        <p class="defaultsection-p1">
+            people pie icing sweet. Brownie jelly-o applicake  applicake sweet 
+            roll liquorice bear claw. Jujubes carrot cake cotton candy sweet 
+            tart brownie. Tiramisu applicake jujubes.
+        </p>
+
+        <p class="defaultsection-p2">
+            Apple pie icing sweet. Brownie jelly-o applicake applicake sweet 
+            roll liquorice bear claw. Jujubes carrot cake cotton candy sweet 
+            tart brownie. Tiramisu applicake jujubes.
+        </p>
+
+        <button class="button-visitMe">
+            Visit me
+        </button>
+
+        </div>
+
+        <div class="col"> 
+        
+            <iframe src="https://player.vimeo.com/video/11893491" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
+            <p><a href="https://vimeo.com/11893491"></a></p>
+
+        </div>
+    </div>
+    
+    <!-- TWITTER -->
+    <div class="row twitter-section">
+
+        <div class="row">
+
+            <div class="col-sm-11 twiter"> twiter </div>
+            <div class="col-sm-1 logo-T"> <img src="imgs/twitter.png"> </div>
+            
+
+        </div>
+
+        <div class="row"> 
+            
+            <div class="col"> <p class="twiter-text">cookie liquorice. Cake donut cupcake lollipop souffle candy. chocolate oat cake</p> </div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="col"> <p class="chessecake">@cheesecake tootsie roll.</p> </div>
+
+        </div>
+
+    </div>
+
+    <!-- informacion de john -->
+    <div class="row who-row">
+        <div class="title-who"> who is john doe</div>
+
+        <div class="col-sm-6 text-who"> bear claw marzipan bear claw applicake i love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps i love unerwear.com souffle cotton candy desser candy ice cream wafer gummies cheesecake brownie</div>
+        <div class="col-sm-6 text-who"> Pudding desser jujubes tiramisu gingerbread croissant tiramisu aplicake. Sesame snaps sugar plum cotton candy chocolate bonbon lemon drops candy cannes cotton candy. cake toffe pie bear claw pastry</div>
+        <div class="col-sm-6 text-who"> muffin chupa chuips jelly beans sweet pie applicake. croissant chocolate cake i love pudding ice cram i love powder pudding appple pie marshmallow. cupcake marzipan oat cake bonbon i love candy canes toffe</div>
+        <div class="col-sm-6"> <img class="logos-who" src="imgs/twitter.png"> <img class="logos-who" src="imgs/gplus.png"> <img class="logos-who" src="imgs/dribble.png"> <img class="logos-who" src="imgs/coffee.png"></div>
+    </div>
+
+
+    <!-- TESTIMONIO -->
+    <div class="row testi-row">
+
+        <div class="row">
+
+            <div class="col-sm-11 testi-title"> Testimonials </div>
+            <div class="col-sm-1 comillas"> " </div>
+
+        </div>
+
+        <div class="row testi-text"> 
+            
+            <div class="col"> Applicake chocolate cake wafer toffe pie souffle water . tat marshmallow wafer macaron chesecake jelly . gingerbread cookie soufle sweet roll sweet roll jelly-o</div>
+
+        </div>
+
+        <div class="row signature">
+
+            <div class="col"> -walter white </div>
+
+        </div>
+
+    </div>
+
+    <!-- Contacto -->
+    <div class="row contacto-zone">
+
+        <p class="Contact">CONTACT</p>
+
+        <div class="col-sm-6 contact-lines">
+            <p class="title-contact">Full Name</p>
+            <input class="box" type="text" placeholder="">
+        </div>
+
+        <div class="col-sm-6 information-contact"> jujubes bear claw croissant wafer sweet lemon drops brownie chocolate bar. sweet lollipop halvah tootsie roll cake fruitcake. chocolate bar gingerbread lollipop tart. jujubes bear claw donut chupa chups.</div>
+       
+        <div class="col-sm-6 contact-lines">
+            <p class="title-contact">Email</p>
+            <input class="box" type="text" placeholder="">
+        </div>
+
+        <div class="col-sm-6 information-contact">
+            <div> linda newman </div>
+            <div> 325 state road 123 </div>
+            <div> mondovi wi wiscoins (98746-54321)</div>
+            <div> (7155)946-1234</div>
+        </div>
+    
+        <div class="row">
+            <p class="title-contact">MESAGGE</p>
+            <textarea class="box-message" name="" id="" cols="30" rows="10"></textarea>
+            
+        </div>
+
+        <input class="btn-mesagge" type="submit" value="MESSAGE">
+    
+    </div>
+
+    <!-- links de abajo  -->
+    <footer>
+
+        <ul>
+            <li class="options"><a class="link-footer" href="#">Home</a></li>
+            <li class="options"><a class="link-footer" href="#">Services</a></li>
+            <li class="options"><a class="link-footer" href="#">Portafolio</a></li>
+            <li class="options"><a class="link-footer" href="#">Contact</a></li>
+        </ul>
+
+    </footer>
+
+</body>
+
+</html>
